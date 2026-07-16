ce5e7846327fa7b669dd4bd9c51d3ad0547b35e1
Hafeez ur rehman <hafiz.hafeez.ur.rehman2022@gmail.com>
2026-07-16 20:34:26 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'khaata-cache-v1';
+const CORE_ASSETS = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((res) => {
+        // don't try to cache cross-origin (fonts/CDN/firebase) responses that fail opaque checks
+        if (event.request.method === 'GET' && res.ok && event.request.url.startsWith(self.location.origin)) {
+          const resClone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+        }
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
